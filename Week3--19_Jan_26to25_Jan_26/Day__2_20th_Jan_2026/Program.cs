e4cbb6a79e9d66a5fb988d44ba5f37a57c12ff9d
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:21:46 +0530

    Add files via upload

--- Week3--19_Jan_26to25_Jan_26/Day__2_20th_Jan_2026/Program.cs
+++ Week3--19_Jan_26to25_Jan_26/Day__2_20th_Jan_2026/Program.cs
@@ -1,46 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 
-class MinimalOperations
+class QuestionPaperMarks
 {
     static void Main()
     {
-        int N = int.Parse(Console.ReadLine());
+        int X = int.Parse(Console.ReadLine());
+        int Y = int.Parse(Console.ReadLine());
+        int N1 = int.Parse(Console.ReadLine());
+        int N2 = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
 
-        // BFS Queue: (current value, steps)
-        Queue<(int value, int steps)> queue = new Queue<(int, int)>();
-        HashSet<int> visited = new HashSet<int>();
+        bool isValid = false;
+        int bestA = 0, bestB = 0;
 
-        queue.Enqueue((10, 0));
-        visited.Add(10);
-
-        while (queue.Count > 0)
+        // Try maximum Type 1 questions first
+        for (int a = N1; a >= 0; a--)
         {
-            var (current, steps) = queue.Dequeue();
+            int remaining = M - (a * X);
 
-            // If target reached
-            if (current == N)
+            if (remaining < 0)
+                continue;
+
+            if (remaining % Y == 0)
             {
-                Console.WriteLine(steps);
-                return;
-            }
-
-            // Possible operations
-            int[] nextValues = {
-                current + 2,
-                current - 1,
-                current * 3
-            };
-
-            foreach (int next in nextValues)
-            {
-                // Limit range to avoid infinite growth
-                if (next >= 0 && next <= 3 * N && !visited.Contains(next))
+                int b = remaining / Y;
+                if (b <= N2)
                 {
-                    visited.Add(next);
-                    queue.Enqueue((next, steps + 1));
+                    isValid = true;
+                    bestA = a;
+                    bestB = b;
+                    break;
                 }
             }
+        }
+
+        if (isValid)
+        {
+            Console.WriteLine("Valid");
+            Console.WriteLine(bestA);
+            Console.WriteLine(bestB);
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
         }
     }
 }
