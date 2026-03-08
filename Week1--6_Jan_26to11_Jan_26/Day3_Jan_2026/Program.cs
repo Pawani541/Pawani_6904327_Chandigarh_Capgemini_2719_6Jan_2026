74e18799157aeca026064889910f24f55c30da18
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:29:12 +0530

    Add files via upload

--- Week1--6_Jan_26to11_Jan_26/Day3_Jan_2026/Program.cs
+++ Week1--6_Jan_26to11_Jan_26/Day3_Jan_2026/Program.cs
@@ -4,18 +4,13 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("What is the correct way to declare an integer?");
-        Console.WriteLine("a. int 1x=10;");
-        Console.WriteLine("b. int x=10;");
-        Console.WriteLine("c. float x=10.0f;");
-        Console.WriteLine("d. string x=\"10\";");
-
-        Console.Write("Choose your answer letter: ");
-        char choice = char.Parse(Console.ReadLine());
-
-        if (choice == 'b' || choice == 'B')
-            Console.WriteLine("Correct choice!");
-        else
-            Console.WriteLine("Incorrect choice!");
+        for (int i = 7; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
     }
 }
