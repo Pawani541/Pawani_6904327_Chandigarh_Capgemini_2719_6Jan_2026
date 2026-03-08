03574e2be1f48f529a507bb7b6302c48595cbe1b
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:44:23 +0530

    Add files via upload

--- Week1--6_Jan_26to11_Jan_26/Day5_10_Jan_2026/Program.cs
+++ Week1--6_Jan_26to11_Jan_26/Day5_10_Jan_2026/Program.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 
-class PerfectNum
+class ProductDigit
 {
     static void Main()
     {
         Console.Write("Enter number: ");
         int n = int.Parse(Console.ReadLine());
-        int output;
+        int output = 0;
 
-        if (n < 0) output = -2;
+        if (n < 0) output = -1;
+        else if (n % 3 == 0 || n % 5 == 0) output = -2;
         else
         {
-            int sum = 0;
-            for (int i = 1; i < n; i++)
-                if (n % i == 0) sum += i;
-            output = (sum == n) ? 1 : -1;
+            int product = 1, num = n;
+            while (num > 0)
+            {
+                product *= num % 10;
+                num /= 10;
+            }
+            output = (product % 3 == 0 || product % 5 == 0) ? 1 : 0;
         }
         Console.WriteLine("Output: " + output);
     }
