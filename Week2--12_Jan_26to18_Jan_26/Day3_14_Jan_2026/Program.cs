6d560fced7dffedbcc4d35de7c9f463c2ae03da9
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:59:03 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day3_14_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day3_14_Jan_2026/Program.cs
@@ -4,34 +4,79 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Enter a binary number:");
-        int input1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter array size:");
+        int input2 = Convert.ToInt32(Console.ReadLine());
 
-        int output = BinaryToDecimal(input1);
+        int[] input1 = new int[input2];
 
-        Console.WriteLine("Output: " + output);
-    }
-
-    static int BinaryToDecimal(int binary)
-    {
-        if (binary > 11111)
-            return -2;
-
-        int decimalValue = 0;
-        int baseValue = 1;
-
-        while (binary > 0)
+        Console.WriteLine("Enter array elements:");
+        for (int i = 0; i < input2; i++)
         {
-            int digit = binary % 10;
-
-            if (digit != 0 && digit != 1)
-                return -1;
-
-            decimalValue += digit * baseValue;
-            baseValue *= 2;
-            binary /= 10;
+            Console.Write("Element " + (i + 1) + ": ");
+            input1[i] = Convert.ToInt32(Console.ReadLine());
         }
 
-        return decimalValue;
+        Console.WriteLine("Enter element to search and remove:");
+        int input3 = Convert.ToInt32(Console.ReadLine());
+
+        int[] output = SearchRemoveAndSort(input1, input2, input3);
+
+        Console.WriteLine("Output:");
+        for (int i = 0; i < output.Length; i++)
+        {
+            Console.Write(output[i] + " ");
+        }
+    }
+
+    static int[] SearchRemoveAndSort(int[] arr, int size, int search)
+    {
+        if (size < 0)
+            return new int[] { -2 };
+
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] < 0)
+                return new int[] { -1 };
+        }
+
+        int index = -1;
+        for (int i = 0; i < size; i++)
+        {
+            if (arr[i] == search)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1)
+            return new int[] { -3 };
+
+        int[] result = new int[size - 1];
+        int j = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            if (i != index)
+            {
+                result[j++] = arr[i];
+            }
+        }
+
+        // Sorting
+        for (int i = 0; i < result.Length - 1; i++)
+        {
+            for (int k = i + 1; k < result.Length; k++)
+            {
+                if (result[i] > result[k])
+                {
+                    int temp = result[i];
+                    result[i] = result[k];
+                    result[k] = temp;
+                }
+            }
+        }
+
+        return result;
     }
 }
