bc12494bef331c9aec1f9c36d905bd0b4a68cc03
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:55:02 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day2_13_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day2_13_Jan_2026/Program.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Linq;
+﻿namespace internal interface;
 
-class Program
+internal class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        int[] arr = { 7, 3, 9, 1, 8, 6 };
-        int size = arr.Length;
-
-        if (size < 0)
-        {
-            Console.WriteLine("-1");
-            return;
-        }
-
-        int mid = size / 2;
-        int[] firstHalf = arr.Take(mid).OrderBy(x => x).ToArray();
-        int[] secondHalf = arr.Skip(mid).OrderByDescending(x => x).ToArray();
-
-        int[] output = firstHalf.Concat(secondHalf).ToArray();
-        Console.WriteLine("Output: " + string.Join(",", output));
+        Console.WriteLine("Hello, World!");
     }
 }
