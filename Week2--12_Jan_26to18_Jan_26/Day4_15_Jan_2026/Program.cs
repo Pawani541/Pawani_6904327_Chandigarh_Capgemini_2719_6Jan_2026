dd8f7e6788be50aa117d2f2dcf7601c82d358906
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:08:16 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
@@ -1,27 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Enter first number: ");
-        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter second number: ");
-        int b = Convert.ToInt32(Console.ReadLine());
+        if (n < 0) 
+        { 
+            Console.WriteLine(-1); 
+            return;
+        }
+        if (n == 0) 
+        {
+            Console.WriteLine(0);
+            return;
+        }
 
-        if (a < 0 || b < 0) { Console.WriteLine(-1); return; }
+        List<int> bits = new List<int>();
+        while (n > 0)
+        {
+            bits.Add(n % 2);
+            n /= 2;
+        }
+        bits.Reverse();
 
-        Console.Write("Enter operation (1=+,2=-,3=*,4=/): ");
-        int op = Convert.ToInt32(Console.ReadLine());
-
-        int result = 0;
-
-        if (op == 1) result = a + b;
-        else if (op == 2) result = a - b;
-        else if (op == 3) result = a * b;
-        else if (op == 4) result = b != 0 ? a / b : 0;
-
-        Console.WriteLine(result);
+        for (int i = 0; i < bits.Count; i++)
+            Console.Write(bits[i] + " ");
     }
 }
