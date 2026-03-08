63e088fe879a05665d8a69b13fc6477177f323c4
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:09:16 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
@@ -4,29 +4,14 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter basic salary: ");
-        int basic = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter year: ");
+        int y = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter working days: ");
-        int days = Convert.ToInt32(Console.ReadLine());
+        if (y < 0) { Console.WriteLine(-1); return; }
 
-        if (basic < 0) 
-        {
-            Console.WriteLine(-1); return;
-        }
-        if (basic > 10000) 
-        {
-            Console.WriteLine(-2); return;
-        }
-        if (days > 31) 
-        {
-            Console.WriteLine(-3); return;
-        }
-
-        double da = 0.75 * basic;
-        double hra = 0.50 * basic;
-        double gross = basic + da + hra;
-
-        Console.WriteLine(gross);
+        if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0))
+            Console.WriteLine("Leap Year");
+        else
+            Console.WriteLine("Not Leap Year");
     }
 }
